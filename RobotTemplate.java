331f8f773496a067102a3c0f334e18c1d6589f10
Caleb Eby <caleb.eby01@gmail.com>
2015-11-03 18:23:33 -0800

    add joystick to solenoid/compressor in teleop

--- RobotTemplate.java
+++ RobotTemplate.java
@@ -24,21 +24,22 @@ import edu.wpi.first.wpilibj.Relay;
  */
 public class RobotTemplate extends SimpleRobot {
     //This function is called once each time the robot enters autonomous mode.
-    Joystick joystick = new Joystick(1);
+    Joystick controller = new Joystick(1);
     
     //DoubleSolenoid doublesolenoid = new DoubleSolenoid(1,2);
-    Relay relay = new Relay(1);
-    Solenoid s1 = new Solenoid(1);
-    Solenoid s2 = new Solenoid(2);
+    Relay compressor = new Relay(1);
+    //Solenoid s1 = new Solenoid(1);
+    //Solenoid s2 = new Solenoid(2);
     DigitalInput input = new DigitalInput(1);
+    DoubleSolenoid launcher = new DoubleSolenoid(1,2);
+    //this runs once
     public void autonomous() {
-        //double value;
-        //boolean buttonvalue = joystick.getRawButton(1);
-        //doublesolenoid.set(DoubleSolenoid.Value.kOff);
-        s1.set(false);
-        s2.set(false);
+        
+        /*launcher.set(DoubleSolenoid.Value.kOff);
+        //s1.set(false);
+        //s2.set(false);
         System.out.println("turned off solenoid");
-        relay.set(Relay.Value.kForward);
+        compressor.set(Relay.Value.kForward);
         System.out.println("turned on relay");
         Timer.delay(1);
         while(!input.get()){}
@@ -51,19 +52,19 @@ public class RobotTemplate extends SimpleRobot {
         while(!input.get()){}
         System.out.println("Button was pressed2nd time ");
         //doublesolenoid
-        s1.set(true);
+        //s1.set(true);
         System.out.println("Solenoid set forward");
         Timer.delay(1);
         while(input.get()){}
         while(!input.get()){}
-        s1.set(false);
-        s2.set(true);
+        //s1.set(false);
+        //s2.set(true);
         System.out.println("button pressed 3 and solenoid set to reverse");
         Timer.delay(1);
         while(input.get()){}
         while(!input.get()){}
-        s2.set(true);
-        s1.set(true);
+        //s2.set(true);
+        //s1.set(true);
         System.out.println("pressed 4, now both on");
 
         Timer.delay(1);
@@ -71,10 +72,10 @@ public class RobotTemplate extends SimpleRobot {
         //doublesolenoid.set(DoubleSolenoid.Value.kReverse);
         while(input.get()){}
         while(!input.get()){}
-        s2.set(false);
-        s1.set(false);
-        relay.set(Relay.Value.kOff);
-        System.out.println("pressed 4, now turned off");
+        //s2.set(false);
+        //s1.set(false);
+        compressor.set(Relay.Value.kOff);
+        System.out.println("pressed 4, now turned off");*/
             
         
 //while(true){
@@ -108,9 +109,23 @@ public class RobotTemplate extends SimpleRobot {
      * This function is called once each time the robot enters operator control.
      */
     public void operatorControl() {
-
+        //Define button
+        boolean a = controller.getRawButton(1);
+        //Turn on compressor and wait 5 seconds then turn off
+        compressor.set(Relay.Value.kForward);
+        System.out.println("Filling air tank");
+        Timer.delay(5.0);
+        compressor.set(Relay.Value.kOff);
+        //Set launcher to reverse
+        launcher.set(DoubleSolenoid.Value.kReverse);
+        System.out.println("Ready to launch");
+        //Wait for buttonpress
+        while(a == false){}
+        //Launch
+        launcher.set(DoubleSolenoid.Value.kForward);
+        System.out.println("Launching");
     }
-
+        
     /**
      * This function is called once each time the robot enters test mode.
      */
