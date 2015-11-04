823c60b85922cf98886c80d4c82d19e130ff46da
Caleb Eby <caleb.eby01@gmail.com>
2015-11-03 18:32:37 -0800

    minor update

--- RobotTemplate.java
+++ RobotTemplate.java
@@ -4,9 +4,7 @@
 /* must be accompanied by the FIRST BSD license file in the root directory of */
 /* the project.                                                               */
 /*----------------------------------------------------------------------------*/
-
 package edu.wpi.first.wpilibj.templates;
-
 
 import edu.wpi.first.wpilibj.DigitalInput;
 import edu.wpi.first.wpilibj.DoubleSolenoid;
@@ -15,6 +13,7 @@ import edu.wpi.first.wpilibj.Solenoid;
 import edu.wpi.first.wpilibj.Timer;
 import edu.wpi.first.wpilibj.Joystick;
 import edu.wpi.first.wpilibj.Relay;
+
 /**
  * The VM is configured to automatically run this class, and to call the
  * functions corresponding to each mode, as described in the SimpleRobot
@@ -23,85 +22,83 @@ import edu.wpi.first.wpilibj.Relay;
  * directory.
  */
 public class RobotTemplate extends SimpleRobot {
+
     //This function is called once each time the robot enters autonomous mode.
     Joystick controller = new Joystick(1);
-    
+
     //DoubleSolenoid doublesolenoid = new DoubleSolenoid(1,2);
     Relay compressor = new Relay(1);
     //Solenoid s1 = new Solenoid(1);
     //Solenoid s2 = new Solenoid(2);
     DigitalInput input = new DigitalInput(1);
-    DoubleSolenoid launcher = new DoubleSolenoid(1,2);
+    DoubleSolenoid launcher = new DoubleSolenoid(1, 2);
+
     //this runs once
     public void autonomous() {
-        
-        /*launcher.set(DoubleSolenoid.Value.kOff);
-        //s1.set(false);
-        //s2.set(false);
-        System.out.println("turned off solenoid");
-        compressor.set(Relay.Value.kForward);
-        System.out.println("turned on relay");
-        Timer.delay(1);
-        while(!input.get()){}
-//        relay.set(Relay.Value.kOff);
-        System.out.println("turned off relay");
-        Timer.delay(1);
-        while(input.get()){}
-        System.out.println("Button no longer pressed");
-        Timer.delay(1);
-        while(!input.get()){}
-        System.out.println("Button was pressed2nd time ");
-        //doublesolenoid
-        //s1.set(true);
-        System.out.println("Solenoid set forward");
-        Timer.delay(1);
-        while(input.get()){}
-        while(!input.get()){}
-        //s1.set(false);
-        //s2.set(true);
-        System.out.println("button pressed 3 and solenoid set to reverse");
-        Timer.delay(1);
-        while(input.get()){}
-        while(!input.get()){}
-        //s2.set(true);
-        //s1.set(true);
-        System.out.println("pressed 4, now both on");
 
-        Timer.delay(1);
+        /*launcher.set(DoubleSolenoid.Value.kOff);
+         //s1.set(false);
+         //s2.set(false);
+         System.out.println("turned off solenoid");
+         compressor.set(Relay.Value.kForward);
+         System.out.println("turned on relay");
+         Timer.delay(1);
+         while(!input.get()){}
+         //        relay.set(Relay.Value.kOff);
+         System.out.println("turned off relay");
+         Timer.delay(1);
+         while(input.get()){}
+         System.out.println("Button no longer pressed");
+         Timer.delay(1);
+         while(!input.get()){}
+         System.out.println("Button was pressed2nd time ");
+         //doublesolenoid
+         //s1.set(true);
+         System.out.println("Solenoid set forward");
+         Timer.delay(1);
+         while(input.get()){}
+         while(!input.get()){}
+         //s1.set(false);
+         //s2.set(true);
+         System.out.println("button pressed 3 and solenoid set to reverse");
+         Timer.delay(1);
+         while(input.get()){}
+         while(!input.get()){}
+         //s2.set(true);
+         //s1.set(true);
+         System.out.println("pressed 4, now both on");
+
+         Timer.delay(1);
         
-        //doublesolenoid.set(DoubleSolenoid.Value.kReverse);
-        while(input.get()){}
-        while(!input.get()){}
-        //s2.set(false);
-        //s1.set(false);
-        compressor.set(Relay.Value.kOff);
-        System.out.println("pressed 4, now turned off");*/
-            
-        
+         //doublesolenoid.set(DoubleSolenoid.Value.kReverse);
+         while(input.get()){}
+         while(!input.get()){}
+         //s2.set(false);
+         //s1.set(false);
+         compressor.set(Relay.Value.kOff);
+         System.out.println("pressed 4, now turned off");*/
 //while(true){
             /*solenoid.set(true);
-            Timer.delay(.1);
-            solenoid.set(false);
-            Timer.delay(.1);*/
+         Timer.delay(.1);
+         solenoid.set(false);
+         Timer.delay(.1);*/
             //buttonvalue = joystick.getRawButton(1);
-            //System.out.println(buttonvalue);
-            //value = joystick.getRawAxis(2);
-            //Timer.delay(.1);
-           // if(input.get()){
-             //   relay.set(Relay.Value.kForward);
-                
-            //}
-            //else(){
-                
-            //    relay.set(Relay.Value.kOff);
-            //}
+        //System.out.println(buttonvalue);
+        //value = joystick.getRawAxis(2);
+        //Timer.delay(.1);
+        // if(input.get()){
+        //   relay.set(Relay.Value.kForward);
+        //}
+        //else(){
+        //    relay.set(Relay.Value.kOff);
+        //}
             /*relay.set(Relay.Value.kForward);
-            Timer.delay(.1);
-            relay.set(Relay.Value.kOff);
-            Timer.delay(.1);
-            relay.set(Relay.Value.kReverse);
-            Timer.delay(.1);
-            relay.set(Relay.Value.kOn);*/
+         Timer.delay(.1);
+         relay.set(Relay.Value.kOff);
+         Timer.delay(.1);
+         relay.set(Relay.Value.kReverse);
+         Timer.delay(.1);
+         relay.set(Relay.Value.kOn);*/
         //}
     }
 
@@ -119,13 +116,25 @@ public class RobotTemplate extends SimpleRobot {
         //Set launcher to reverse
         launcher.set(DoubleSolenoid.Value.kReverse);
         System.out.println("Ready to launch");
-        //Wait for buttonpress
-        while(a == false){}
-        //Launch
-        launcher.set(DoubleSolenoid.Value.kForward);
-        System.out.println("Launching");
+        while (true) {
+            //Wait for buttonpress
+            while (!a||!input.get()) {}
+            //Launch
+            launcher.set(DoubleSolenoid.Value.kForward);
+            System.out.println("Launching");
+            //Wait 3 seconds, then reverse
+            Timer.delay(3);
+            System.out.println("Re-setting launcher");
+            launcher.set(DoubleSolenoid.Value.kReverse);
+            //Turn on compressor for 5 seconds
+            compressor.set(Relay.Value.kForward);
+            System.out.println("Filling air tank");
+            Timer.delay(5);
+            compressor.set(Relay.Value.kOff);
+            System.out.println("Ready to launch");
+        }
     }
-        
+
     /**
      * This function is called once each time the robot enters test mode.
      */
