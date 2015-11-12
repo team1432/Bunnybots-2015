9d9ac8ff75dccf2274e2a4b62513d8bc390c8c61
calebeby <caleb.eby01@gmail.com>
2015-11-11 17:52:09 -0800

    update

--- RobotTemplate.java
+++ RobotTemplate.java
@@ -14,6 +14,8 @@ import edu.wpi.first.wpilibj.Timer;
 import edu.wpi.first.wpilibj.Joystick;
 import edu.wpi.first.wpilibj.Relay;
 import edu.wpi.first.wpilibj.Compressor;
+import edu.wpi.first.wpilibj.Jaguar;
+import edu.wpi.first.wpilibj.RobotDrive;
 import edu.wpi.first.wpilibj.Victor;
 
 /**
@@ -30,94 +32,39 @@ public class RobotTemplate extends SimpleRobot {
     DoubleSolenoid launcher = new DoubleSolenoid(2, 1);
     Compressor compressor = new Compressor(1, 1);
     Victor launchwheels = new Victor(1);
-
+    Jaguar leftwheel, rightwheel, centerwheel;
+    RobotDrive drive;
     //this runs once
     public void autonomous() {
-
-	/*launcher.set(DoubleSolenoid.Value.kOff);
-	 //s1.set(false);
-	 //s2.set(false);
-	 System.out.println("turned off solenoid");
-	 compressor.set(Relay.Value.kForward);
-	 System.out.println("turned on relay");
-	 Timer.delay(1);
-	 while(!input.get()){}
-	 //        relay.set(Relay.Value.kOff);
-	 System.out.println("turned off relay");
-	 Timer.delay(1);
-	 while(input.get()){}
-	 System.out.println("Button no longer pressed");
-	 Timer.delay(1);
-	 while(!input.get()){}
-	 System.out.println("Button was pressed2nd time ");
-	 //doublesolenoid
-	 //s1.set(true);
-	 System.out.println("Solenoid set forward");
-	 Timer.delay(1);
-	 while(input.get()){}
-	 while(!input.get()){}
-	 //s1.set(false);
-	 //s2.set(true);
-	 System.out.println("button pressed 3 and solenoid set to reverse");
-	 Timer.delay(1);
-	 while(input.get()){}
-	 while(!input.get()){}
-	 //s2.set(true);
-	 //s1.set(true);
-	 System.out.println("pressed 4, now both on");
-
-	 Timer.delay(1);
-        
-	 //doublesolenoid.set(DoubleSolenoid.Value.kReverse);
-	 while(input.get()){}
-	 while(!input.get()){}
-	 //s2.set(false);
-	 //s1.set(false);
-	 compressor.set(Relay.Value.kOff);
-	 System.out.println("pressed 4, now turned off");*/
-//while(true){
-            /*solenoid.set(true);
-	 Timer.delay(.1);
-	 solenoid.set(false);
-	 Timer.delay(.1);*/
-            //buttonvalue = joystick.getRawButton(1);
-	//System.out.println(buttonvalue);
-	//value = joystick.getRawAxis(2);
-	//Timer.delay(.1);
-	// if(input.get()){
-	//   relay.set(Relay.Value.kForward);
-	//}
-	//else(){
-	//    relay.set(Relay.Value.kOff);
-	//}
-            /*relay.set(Relay.Value.kForward);
-	 Timer.delay(.1);
-	 relay.set(Relay.Value.kOff);
-	 Timer.delay(.1);
-	 relay.set(Relay.Value.kReverse);
-	 Timer.delay(.1);
-	 relay.set(Relay.Value.kOn);*/
-	//}
     }
 
     /**
      * This function is called once each time the robot enters operator control.
      */
-    public void teleoptInit(){
-	System.out.println("teleop started");
+    public void robotInit() {
+	System.out.println("Ready to start");
+	setupdrive();
     }
+
+    public void disabled() {
+	System.out.println("Robot Disabled");
+    }
+
     public void operatorControl() {
+	System.out.println("Robot Enabled");
+	while (true){
+	    drive.arcadeDrive(controller);
+	}
+	/*Timer.delay(0.2);
 	compressor.start();
 	System.out.println("started compressor");
 	//Define button
 	boolean a = controller.getRawButton(1);
 	boolean b = controller.getRawButton(2);
-
 	//Set launcher to reverse
-	launcher.set(DoubleSolenoid.Value.kReverse);
+	launcherreverse();
 	System.out.println("Ready to launch");
 	while (isOperatorControl() && isEnabled()) {
-	    Timer.delay(.2);
 	    //Wait for buttonpress
 	    a = controller.getRawButton(1);
 	    b = controller.getRawButton(2);
@@ -128,35 +75,68 @@ public class RobotTemplate extends SimpleRobot {
 	    }
 	    if (a) {
 		System.out.println("A pressed");
-		//Launch
-		launchwheels.set(0.5);
-		Timer.delay(0.2);
-		launcher.set(DoubleSolenoid.Value.kForward);
-		System.out.println("Launching");
-		//Wait 1 second, then reverse
-		Timer.delay(1);
-		launchwheels.set(0);
-		System.out.println("Re-setting launcher");
-		launcher.set(DoubleSolenoid.Value.kReverse);
+		autolaunch(1);
 	    }
 	    if (b) {
+		while (b) {
+		    Timer.delay(.1);
+		    b = controller.getRawButton(2);
+		}
 		System.out.println("B pressed");
 		if (launcher.get() == DoubleSolenoid.Value.kReverse) {
-		    launcher.set(DoubleSolenoid.Value.kForward);
-		    System.out.println("forward");
+		    launcherforward();
 		} else {
-		    //Timer.delay(.1);
-		    launcher.set(DoubleSolenoid.Value.kReverse);
-		    System.out.println("reverse");
+		    launcherreverse();
 		}
 	    }
 
-	}
+	}*/
     }
+
     /**
      * This function is called once each time the robot enters test mode.
      */
     public void test() {
 
     }
+
+    /*
+     This sets the shooter on solenoid 2,1 forward
+     */
+    public void launcherforward() {
+	launcher.set(DoubleSolenoid.Value.kForward);
+	System.out.println("forward");
+    }
+
+    /**
+     * sets shooter on solenoid 2,1 reverse
+     */
+    public void launcherreverse() {
+	launcher.set(DoubleSolenoid.Value.kReverse);
+	System.out.println("reverse");
+    }
+    /**
+     * autolaunch sends launcher forward, turns motors on, and then off, and reverse
+     * @param button is the integer of the button on the controller. 
+     */
+    public void autolaunch(int button) {
+	boolean a = controller.getRawButton(button);
+	launchwheels.set(0.3);
+	//Launch
+	Timer.delay(0.2);
+	launcherforward();
+	while (a) {
+	    a = controller.getRawButton(button);
+	    Timer.delay(0.1);
+	}
+	launchwheels.set(0);
+	launcherreverse();
+    }
+    public void setupdrive(){
+	leftwheel = new Jaguar(3);
+	centerwheel = new Jaguar(4);
+	rightwheel = new Jaguar(5);
+	drive = new RobotDrive(leftwheel, rightwheel);
+    }
+    
 }
